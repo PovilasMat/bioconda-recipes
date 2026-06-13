a51e84ba0a75423c76022583091a73dd54acf03a
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2026-06-13 17:07:44 +0930

    Update recipe

--- recipes/kmertools/build.sh
+++ recipes/kmertools/build.sh
@@ -3,14 +3,19 @@
 set -exuo pipefail
 
 export CARGO_NET_GIT_FETCH_WITH_CLI=true
+export CARGO_NET_RETRY=10
 export CARGO_HOME="${BUILD_PREFIX}/.cargo"
 export RUST_BACKTRACE=1
+
+cargo fetch --locked
+export CARGO_NET_OFFLINE=true
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 cargo install \
     --verbose \
     --locked \
+    --offline \
     --no-track \
     --path kmertools \
     --root "${PREFIX}"
@@ -21,7 +26,8 @@ maturin build \
     --interpreter "${PYTHON}" \
     --release \
     --strip \
-    --locked
+    --locked \
+    --offline
 
 "${PYTHON}" -m pip install \
     ./target/wheels/*.whl \
