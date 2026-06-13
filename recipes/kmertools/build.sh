e1963b375c94502f706b0b40ced4ae5db3c5b17d
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2026-06-13 15:23:31 +0930

    Update recipe

--- recipes/kmertools/build.sh
+++ recipes/kmertools/build.sh
@@ -1,37 +1,31 @@
-#!/bin/bash -e
+#!/bin/bash
 
-# Build with Rust
-cargo build --release
+set -exuo pipefail
 
-# Install the binaries
-cargo install --verbose --path kmertools --root "${PREFIX}" 
+export CARGO_NET_GIT_FETCH_WITH_CLI=true
+export CARGO_HOME="${BUILD_PREFIX}/.cargo"
+export RUST_BACKTRACE=1
 
-# Check if the system is macOS
-if [[ "$(uname)" == "Darwin" ]]; then
-    # Get the architecture
-    arch=$(uname -m)
-    
-    # Set MACOSX_DEPLOYMENT_TARGET based on architecture
-    if [[ "$arch" == "x86_64" ]]; then
-        # For Intel Macs
-        export MACOSX_DEPLOYMENT_TARGET=10.12
-    elif [[ "$arch" == "arm64" ]]; then
-        # For Apple Silicon Macs
-        export MACOSX_DEPLOYMENT_TARGET=11.0
-    else
-        echo "Unknown architecture: $arch"
-        exit 1
-    fi
-    
-    echo "Set MACOSX_DEPLOYMENT_TARGET to $MACOSX_DEPLOYMENT_TARGET for $arch"
-else
-    echo "Not running on macOS, skipping MACOSX_DEPLOYMENT_TARGET"
-fi
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-# Build statically linked binary with Rust
-RUST_BACKTRACE=1
-# Build with maturin
-maturin build -m ./conda/Cargo.toml -b pyo3 --interpreter "${PYTHON}" --release --strip
+cargo install \
+    --verbose \
+    --locked \
+    --no-track \
+    --path kmertools \
+    --root "${PREFIX}"
 
-# Install the wheel file
-${PYTHON} -m pip install ./target/wheels/*.whl --no-deps --no-build-isolation --no-cache-dir -vvv
+maturin build \
+    --manifest-path ./conda/Cargo.toml \
+    --bindings pyo3 \
+    --interpreter "${PYTHON}" \
+    --release \
+    --strip \
+    --locked
+
+"${PYTHON}" -m pip install \
+    ./target/wheels/*.whl \
+    --no-deps \
+    --no-build-isolation \
+    --no-cache-dir \
+    -vvv
